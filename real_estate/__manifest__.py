384e0e39f885ea10eeac6012a8e1c1325eecf519
RamiroFerreiro <ramiro.x.ferreiro@gmail.com>
2025-11-17 10:14:07 -0300

    Se realiza hasta el ejercicio 16 de la unidad 1 de la guía. Implementaciones relacionadas a permisos de usuarios

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -5,7 +5,8 @@
     'category': 'Real Estate',
     'depends': ['base'],
     'data': [
-
+        "security/real_estate_res_groups.xml",
+        "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/real_estate_menuitem.xml"
 
